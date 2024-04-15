b3d9fa8d82aaa21e5d53100f62ef974cc711658d
IntelPentiumG2 <loliwaffel@gmail.com>
2024-04-15 18:43:00 +0200

    More cs doc

--- SimpleNetworking/EventArgs/ClientDisconnectedEventArgs.cs
+++ SimpleNetworking/EventArgs/ClientDisconnectedEventArgs.cs
@@ -2,10 +2,25 @@
 
 namespace SimpleNetworking.EventArgs
 {
+    /// <summary>
+    /// Event arguments for when a client disconnects
+    /// </summary>
+    /// <param name="socket">The socket that disconnected</param>
+    /// <param name="disconnectionTime">The time the client disconnected</param>
+    /// <param name="reason">The reason for the disconnect</param>
     public class ClientDisconnectedEventArgs(Socket? socket, DateTime disconnectionTime, string reason)
     {
+        /// <summary>
+        /// The socket that disconnected
+        /// </summary>
         public Socket? Socket { get; set; } = socket;
+        /// <summary>
+        /// The time the client disconnected
+        /// </summary>
         public DateTime DisconnectionTime { get; set; } = disconnectionTime;
+        /// <summary>
+        /// The reason for the disconnect
+        /// </summary>
         public string Reason { get; set; } = reason;
     }
 }
--- SimpleNetworking/EventArgs/MessageReceivedEventArgs.cs
+++ SimpleNetworking/EventArgs/MessageReceivedEventArgs.cs
@@ -3,10 +3,24 @@ using System.Text;
 
 namespace SimpleNetworking.EventArgs
 {
+    /// <summary>
+    /// Event arguments for when a message is received
+    /// </summary>
+    /// <param name="buffer">The buffer received</param>
+    /// <param name="remoteEp">The remote endpoint it was received from</param>
     public class MessageReceivedEventArgs(byte[] buffer, IPEndPoint remoteEp)
     {
+        /// <summary>
+        /// The buffer received
+        /// </summary>
         public byte[] Buffer { get; } = buffer;
+        /// <summary>
+        /// The remote endpoint the buffer was received from
+        /// </summary>
         public IPEndPoint Sender { get; } = remoteEp;
+        /// <summary>
+        /// The buffer received as a string
+        /// </summary>
         public string MessageString => Encoding.UTF8.GetString(Buffer);
     }
 }
